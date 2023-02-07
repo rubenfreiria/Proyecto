4c00d6fc2ab07c4fbb7d6fa5be6d4395c07275e2
rubenfreiria <rubenfreiriaiglesias@gmail.com>
2023-02-07 09:38:33 +0100

    Acceso a base de datos hecho
    Se crean las funciones para ver si el formulario de login está correctamente rellenado y tambien la funcion de conexion a la base de datos

--- public/procesoLogin.php
+++ public/procesoLogin.php
@@ -1,11 +1,44 @@
 <?php
-  
-  if (isset($_POST['loginEmail'])&&isset($_POST['loginPassword'])) {
-    $loginEmail = $_POST['loginEmail'];
-    $loginPassword = $_POST['loginPassword'];
-    
-    
+/* Funcion que devuelve true si los campos del formulario están completos */
+function comprobarFormLogin()
+{
+  if ($_POST['loginEmail'] != "" && $_POST['loginPassword'] != "") {
+    return true;
   } else {
-    echo 'No se han recibido datos del formulario';
+    return false;
   }
+}
+
+function comprobarUserEnDB()
+{
+  $resultadoComprobarLogin = comprobarFormLogin();
+  if ($resultadoComprobarLogin == true) {
+    $loginEmail = $_POST['loginEmail'];
+    $loginpasswd = $_POST['loginPassword'];
+    try {
+      $pdo = new PDO('mysql:dbname=protectora_teis;host=localhost', 'root', '');
+    } catch (PDOException $e) {
+      die("ERROR: " . $e->getMessage() . "<br>" . $e->getCode());
+    }
+    $consulta = "SELECT * FROM usuarios WHERE email = '$loginEmail' AND passwd = '$loginpasswd';";
+    $resultado = $pdo->query($consulta);
+    if ($resultado->rowCount() > 0) {
+      //El usuario existe y las credenciales son correctas
+      return true;
+    } else {
+      //El usuario no existe o las credenciales son incorrectas
+      return false;
+    }
+  } else {
+    echo "Los datos introducidos no son correctos";
+    return false;
+  }
+}
+
+$resultadoComprobarUserEnDB = comprobarUserEnDB();
+if ($resultadoComprobarUserEnDB == true) {
+  /* Caso de datos introducidos correctos. Continuar haciendo el inicio de sesion */
+} else {
+  echo "El usuario no existe o las credenciales son incorrectas.";
+}
 ?>
