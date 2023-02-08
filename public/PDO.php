ef54187e42ac61270e54e7a6b433c656ec4cef21
rubenfreiria <rubenfreiriaiglesias@gmail.com>
2023-02-08 10:58:16 +0100

    Cambio de la funcion pdo
    Ahora lee los datos de inicio para la base de datos de un archivo de configuracion

--- public/PDO.php
+++ public/PDO.php
@@ -1,12 +1,42 @@
 <?php
 /* Funcion para conectarse a la base de datos usando PDO */
+function leer_config($fichero_config_BBDD, $esquema) {
+    /*
+     * $fichero_config_BBDD es la ruta del fichero con los datos de conexión a la BBDD
+     * $esquema es la ruta del fichero XSD para validar la estructura del fichero anterior
+     * Si el fichero de configuración existe y es válido, devuelve un array con tres
+     * valores: la cadena de conexión, el nombre de usuario y la clave.
+     * Si no encuentra el fichero o no es válido, lanza una excepción.
+     */
+
+    $config = new DOMDocument();
+    $config->load($fichero_config_BBDD);
+    $res = $config->schemaValidate($esquema);
+    if ($res === FALSE) {
+        throw new InvalidArgumentException("Revise el fichero de configuración");
+    }
+    $datos = simplexml_load_file($fichero_config_BBDD);
+    $ip = $datos->xpath("//ip");
+    $nombre = $datos->xpath("//nombre");
+    $usu = $datos->xpath("//usuario");
+    $clave = $datos->xpath("//clave");
+    $cad = sprintf("mysql:dbname=%s;host=%s", $nombre[0], $ip[0]);
+    $resul = [];
+    $resul[] = $cad;
+    $resul[] = $usu[0];
+    $resul[] = $clave[0];
+    return $resul;
+}
+
 function conectarBD()
 {
     try {
-        $pdo = new PDO('mysql:dbname=protectora_teis;host=localhost', 'root', '');
+        $res = leer_config(dirname(__FILE__) . "./config/configuracion.xml", dirname(__FILE__) . "./config/configuracion.xsd");
+        $pdo = new PDO($res[0], $res[1], $res[2]);
+        return $pdo;
     } catch (PDOException $e) {
-        die("ERROR: " . $e->getMessage() . "<br>" . $e->getCode());
+        echo $e->getMessage();
+        exit();
     }
-    return $pdo;
 }
 ?>
