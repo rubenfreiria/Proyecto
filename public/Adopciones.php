cde12454e9347f79093fbc4997c2e2358cdbd966
rubenfreiria <rubenfreiriaiglesias@gmail.com>
2023-02-09 20:54:01 +0100

    Fix link y mas estilos para adopciones

--- public/Adopciones.php
+++ public/Adopciones.php
@@ -15,18 +15,18 @@
             <div id="menuIzquierda">
                 <img class="elementoMenu" id="logoHeader" src="../media/logos/logoWhite.png" />
                 <h3>
-                    <a class="elementoMenu" id="h3Menu" href="index.php">Protectora</a>
+                    <a class="elementoMenu" id="h3Menu" href="../index.php">Protectora</a>
                 </h3>
             </div>
             <div id="menuDerecha">
-                <a class="elementoMenu" href="public/register.php">Registrarse</a>
-                <a class="elementoMenu" href="public/login.php">Login</a>
+                <a class="elementoMenu" href="../public/register.php">Registrarse</a>
+                <a class="elementoMenu" href="../public/login.php">Login</a>
             </div>
         </nav>
     </header>
     <section>
         <div id="menuContainer">
-            <a class="menuLink" href="">Adopciones</a>
+            <a class="menuLink" href="./adopciones.php">Adopciones</a>
             <a class="menuLink" href="">Donaciones</a>
             <a class="menuLink" href="">Noticias</a>
             <a class="menuLink" href="">Calendario</a>
@@ -34,43 +34,43 @@
         </div>
     </section>
     <div id="bodyAdopciones">
-    <h1>Adopciones</h1>
-    <main id="mainAdopciones">
-        <?php
-        include('../modules/PDO.php');
-        $pdo = conectarBD("otro");
-        $consulta = "SELECT nombre, genero, raza, fecha_nacimiento, foto FROM animales;";
-        $resultado = $pdo->query($consulta);
-        $datosAnimal = $resultado->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($datosAnimal as $animal) {
-            echo "<div class='animal'>";
-            echo "<div class='animalImagen'>";
-            echo "<img src=" . $animal['foto'] . " alt='foto_animal' />";
-            echo "</div>";
-            echo "<div class='animalInfo'>";
-            echo "<p id='animalNombre'><b>" . $animal['nombre'] . "</b></p>";
-            echo "<p>Raza: <b>" . $animal['raza'] . "</b></p>";
-            echo "<p>Sexo: <b>" . $animal['genero'] . "</b></p>";
-            echo "<p>Fecha nacimiento: <b>" . $animal['fecha_nacimiento'] . "</b></p>";
-            echo "<div id='divBtnAdoptar'><button><b>Adoptar</b></button></div>";
-            echo "</div>";
-            echo "</div>";
-        }
-        ?>
-    </main>
+        <h1>Adopciones</h1>
+        <main id="mainAdopciones">
+            <?php
+            include('../modules/PDO.php');
+            $pdo = conectarBD("otro");
+            $consulta = "SELECT nombre, genero, raza, fecha_nacimiento, foto FROM animales;";
+            $resultado = $pdo->query($consulta);
+            $datosAnimal = $resultado->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($datosAnimal as $animal) {
+                echo "<div class='animal'>";
+                echo "<div class='animalImagen'>";
+                echo "<img src=" . $animal['foto'] . " alt='foto_animal' />";
+                echo "</div>";
+                echo "<div class='animalInfo'>";
+                echo "<p id='animalNombre'><b>" . $animal['nombre'] . "</b></p>";
+                echo "<p>Raza: <b>" . $animal['raza'] . "</b></p>";
+                echo "<p>Sexo: <b>" . $animal['genero'] . "</b></p>";
+                echo "<p>Fecha nacimiento: <b>" . $animal['fecha_nacimiento'] . "</b></p>";
+                echo "<div id='divBtnAdoptar'><button><b>Adoptar</b></button></div>";
+                echo "</div>";
+                echo "</div>";
+            }
+            ?>
+        </main>
     </div>
     <footer id="footer-index">
-      <div id="social">
-        <a>
-          <img id="twitter" src="../media/social/instagram.svg" alt="twitter" />
-        </a>
-        <a>
-          <img id="instagram" src="../media/social/tiktok.svg" alt="instagram" />
-        </a>
-        <a>
-          <img id="tiktok" src="../media/social/twitter.svg" alt="tiktok" />
-        </a>
-      </div>
+        <div id="social">
+            <a>
+                <img id="twitter" src="../media/social/instagram.svg" alt="twitter" />
+            </a>
+            <a>
+                <img id="instagram" src="../media/social/tiktok.svg" alt="instagram" />
+            </a>
+            <a>
+                <img id="tiktok" src="../media/social/twitter.svg" alt="tiktok" />
+            </a>
+        </div>
     </footer>
 </body>
 
