7218902e91d2e592a5c4bb27e3bf4437047d8338
rubenfreiria <rubenfreiriaiglesias@gmail.com>
2023-02-09 16:47:44 +0100

    Comentarios doxyfile para PDO

--- modules/PDO.php
+++ modules/PDO.php
@@ -1,15 +1,14 @@
 <?php
-/* Funcion para conectarse a la base de datos usando PDO */
+/**
+ * Funcion que lee la configuracion de los parametros de ficheros XML
+ *
+ * @param   [xml file]  $fichero_config_BBDD    [fichero xml con los parametros para conectarse a la base de datos
+ * @param   [xsd file]  $esquema                [fichero xsd que comprueba que el xml esta bien formado]
+ *
+ * @return  [array]                             [array con los valores de conexion a la base de datos]
+ */
 function leer_config($fichero_config_BBDD, $esquema)
 {
-    /*
-     * $fichero_config_BBDD es la ruta del fichero con los datos de conexión a la BBDD
-     * $esquema es la ruta del fichero XSD para validar la estructura del fichero anterior
-     * Si el fichero de configuración existe y es válido, devuelve un array con tres
-     * valores: la cadena de conexión, el nombre de usuario y la clave.
-     * Si no encuentra el fichero o no es válido, lanza una excepción.
-     */
-
     $config = new DOMDocument();
     $config->load($fichero_config_BBDD);
     $res = $config->schemaValidate($esquema);
@@ -30,6 +29,14 @@ function leer_config($fichero_config_BBDD, $esquema)
 }
 
 
+/**
+ * Recibe una cadena con los permisos de la conexion que se desean
+ * Devuelve un objeto pdo listo para lanzar consultas a la base de datos con el usuario indicado
+ *
+ * @param   [string]  $permisos  [cadena que corresponda con los permisos que se deseen acceder a la BBDD]
+ *
+ * @return  [PDO object]             [return objeto PDO con los permisos selecionados]
+ */
 function conectarBD($permisos)
 {
     try {
@@ -38,14 +45,15 @@ function conectarBD($permisos)
         if ($permisos === "admin") {
             $res = leer_config(dirname(__FILE__) . "./config/configuracionAdmin.xml", dirname(__FILE__) . "./config/configuracion.xsd");
             $pdo = new PDO($res[0], $res[1], $res[2]);
-        }
-        if ($permisos === "veterinario") {
+        } else if ($permisos === "veterinario") {
             $res = leer_config(dirname(__FILE__) . "./config/configuracionVeterinario.xml", dirname(__FILE__) . "./config/configuracion.xsd");
             $pdo = new PDO($res[0], $res[1], $res[2]);
-        }
-        if ($permisos === "otro") {
+        } else if ($permisos === "otro") {
             $res = leer_config(dirname(__FILE__) . "./config/configuracionOtro.xml", dirname(__FILE__) . "./config/configuracion.xsd");
             $pdo = new PDO($res[0], $res[1], $res[2]);
+        } else {
+            header("Location: ../public/error.html");
+            die();
         }
         return $pdo;
     } catch (PDOException $e) {
