07e049ae7093dc1a62a2a6db8204d598da264e97
thePOSADA <posadasotosamuel@gmail.com>
2023-03-01 00:13:51 +0100

    Update contacto.php

--- public/contacto.php
+++ public/contacto.php
@@ -13,13 +13,21 @@
     <script src="../js/mapaContacto.js"></script>
 
     <style>
+        #bodyContactos {
+            margin-top: 40px;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+        }
+
         #mainContacto {
             display: flex;
             flex-direction: column;
             text-align: center;
             align-items: center;
             width: 100%;
-            max-width: 1800px;
         }
 
         #contactoNum {
@@ -39,17 +47,18 @@
             flex-wrap: wrap;
             justify-content: center;
             align-items: center;
-            margin: 20px;
             width: 100%;
+            background-color: var(--main-grey);
+            margin-bottom: 50px;
         }
 
         #contMap {
-            height: 400px;
+            height: 500px;
             width: 50%;
             display: flex;
             justify-content: center;
             align-items: center;
-            max-width: 400px;
+            max-width: 500px;
         }
 
         #textMap {
@@ -58,13 +67,23 @@
             display: flex;
             flex-direction: column;
             justify-content: center;
-            max-width: 400px;
+            max-width: 500px;
         }
 
         #map {
             width: 100%;
             height: 100%;
             background-color: grey;
+        }
+
+        #tablaHorario {
+            margin: 20px;
+            border: solid 1px black;
+        }
+
+        thead {
+            height: 50px;
+            border: solid 1px black;
         }
     </style>
 </head>
@@ -102,9 +121,9 @@
             <a class="menuLink" href="../public/contacto.php">Contacto</a>
         </div>
     </section>
-    <div id="bodyAdopciones">
+    <div id="bodyContactos">
         <h1>Contacto</h1>
-        <main id="mainContacto">
+        <div id="mainContacto">
             <div id="contactoNum">
                 <h2>Donde encontrarnos</h2>
                 <p><b>Número de telefono:</b> 690 420 123</p>
@@ -120,7 +139,96 @@
                     <p>36216 Vigo, Pontevedra</p>
                 </div>
             </div>
-        </main>
+        </div>
+        <h1>Horario</h1><br>
+        <div>
+            <table id="tablaHorario">
+                <thead>
+                    <tr>
+                        <th>Hora</th>
+                        <th>Lunes</th>
+                        <th>Martes</th>
+                        <th>Miércoles</th>
+                        <th>Jueves</th>
+                        <th>Viernes</th>
+                        <th>Sábado</th>
+                        <th>Domingo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>9:00 AM</td>
+                        <td rowspan="4"></td>
+                        <td rowspan="2" style="background-color: white"></td>
+                        <td rowspan="4"></td>
+                        <td rowspan="2" style="background-color: white"></td>
+                        <td rowspan="4"></td>
+                        <td rowspan="1" style="background-color: white"></td>
+                        <td rowspan="1" style="background-color: white"></td>
+                    </tr>
+                    <tr>
+                        <td>10:00 AM</td>
+                        <td rowspan="3"></td>
+                        <td rowspan="3"></td>
+                    </tr>
+                    <tr>
+                        <td>11:00 AM</td>
+                        <td rowspan="2"></td>
+                        <td rowspan="2"></td>
+                    </tr>
+                    <tr>
+                        <td>12:00 PM</td>
+                    </tr>
+                    <tr>
+                        <td>1:00 PM</td>
+                        <td rowspan="3"></td>
+                        <td rowspan="3"></td>
+                        <td rowspan="3"></td>
+                        <td rowspan="3"></td>
+                        <td rowspan="3"></td>
+                        <td rowspan="3"></td>
+                        <td rowspan="3"></td>
+                    </tr>
+                    <tr>
+                        <td>2:00 PM</td>
+                    </tr>
+                    <tr>
+                        <td>3:00 PM</td>
+                    </tr>
+                    <tr>
+                        <td>4:00 PM</td>
+                        <td rowspan="3"></td>
+                        <td rowspan="2"></td>
+                        <td rowspan="3"></td>
+                        <td rowspan="2"></td>
+                        <td rowspan="3"></td>
+                        <td rowspan="3"></td>
+                        <td rowspan="6" style="background-color: white"></td>
+                    </tr>
+                    <tr>
+                        <td>5:00 PM</td>
+                    </tr>
+                    <tr>
+                        <td>6:00 PM</td>
+                        <td rowspan="4" style="background-color: white"></td>
+                        <td rowspan="4" style="background-color: white"></td>
+                    </tr>
+                    <tr>
+                        <td>7:00 PM</td>
+                        <td rowspan="3" style="background-color: white"></td>
+                        <td rowspan="3" style="background-color: white"></td>
+                        <td rowspan="3" style="background-color: white"></td>
+                        <td rowspan="3" style="background-color: white"></td>
+                    </tr>
+                    <tr>
+                        <td>8:00 PM</td>
+                    </tr>
+                    <tr>
+                        <td>9:00 PM</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
     <footer id="footer-index">
         <div id="social">
